6b24f1c163d23ce6b5503e86fdf98f23fe19fedf
Ashish Sharda <ashishjsharda@gmail.com>
2024-11-17 23:21:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@services': '/src/services',
-      '@hooks': '/src/hooks',
-      '@utils': '/src/utils',
-      '@context': '/src/context',
-      '@constants': '/src/constants',
-    },
-  },
-  base: './', // Ensures assets are served correctly on deployment
   build: {
-    outDir: 'dist', // Default for Vite, ensures Vercel points to the correct directory
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
+  base: '/',  // This is important for correct asset paths
 });
